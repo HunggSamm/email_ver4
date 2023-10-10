937315dd2bf48bc607efc778ba08ff2f52959b74
HunggSamm <hunggsamm@gmail.com>
2023-10-10 11:57:45 +0700

    delete comment

--- src/main/java/murach/email/EmailListServlet.java
+++ src/main/java/murach/email/EmailListServlet.java
@@ -19,16 +19,14 @@ public class EmailListServlet extends HttpServlet
 
         HttpSession session = request.getSession();
 
-        // get current action
         String action = request.getParameter("action");
         if (action == null) {
-            action = "join";  // default action
+            action = "join";
         }
 
-        // perform action and set URL to appropriate page
         String url = "/index.jsp";
         if (action.equals("join")) {
-            url = "/index.jsp";    // the "join" page
+            url = "/index.jsp";
         }
         else if (action.equals("add")) {
             // get parameters from the request
@@ -36,25 +34,20 @@ public class EmailListServlet extends HttpServlet
             String lastName = request.getParameter("lastName");
             String email = request.getParameter("email");
 
-            // store data in User object and save User object in database
             User user = new User(firstName, lastName, email);
             UserDB.insert(user);
 
-            // set User object in request object and set URL
             session.setAttribute("user", user);
-            url = "/thanks.jsp";   // the "thanks" page
+            url = "/thanks.jsp";
         }
 
-        // create the Date object and store it in the request
         Date currentDate = new Date();
         request.setAttribute("currentDate", currentDate);
 
-        // create users list and store it in the session
         String path = getServletContext().getRealPath("/WEB-INF/EmailList.txt");
         ArrayList<User> users = UserIO.getUsers(path);
         session.setAttribute("users", users);
 
-        // forward request and response objects to specified URL
         getServletContext()
                 .getRequestDispatcher(url)
                 .forward(request, response);
